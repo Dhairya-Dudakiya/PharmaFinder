d36eb9be84952f255ed893540bdfed51168c2cd2
Dhairya Dudakiya <dhairya.dudakiya121451@marwadiuniversity.ac.in>
2025-08-11 21:52:27 +0530

    Refactor build.gradle.kts and AndroidManifest.xml for improved readability and updated compatibility settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,40 +1,28 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services") 
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin") 
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("dev.flutter.flutter-gradle-plugin")
 }
+
 
 android {
     namespace = "com.example.pharmafinder"
-    compileSdk = 35 
+    compileSdk = 35
 
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
         applicationId = "com.example.pharmafinder"
-
-        
         minSdk = 23
-
-        targetSdk = 34 
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug") 
-        }
     }
 }
 
